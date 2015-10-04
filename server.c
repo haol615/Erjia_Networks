d3ccc859fad12a805446a2a9540a80cc294a59c9
Hao Li <haol615@gmail.com>
2015-10-04 14:21:44 -0700

    newer version

--- server.c
+++ server.c
@@ -55,69 +55,68 @@ int main(int argc, const char * argv[]) {
     char *loginid = malloc(255);
     char *name = malloc(255);
     int cookie;
+    char * server_status;
+
     for(;;){
         clntSock = accept(servSock, (struct sockaddr *)&echoClntAddr, &clntlen);
         if(clntSock<0){
             printf("accpet failed!");
-        exit(1);
-    }
+            exit(1);
+        }
 
         //receive MESSAGE
-    int size_recv = recv(clntSock, client_hello, CHUNK_SIZE,0 );
-    char *pch;
-    pch  = strtok(client_hello," ");
-    while(pch != NULL){
-        if(counter == 1){
-            if(strcmp(pch, MAGIC_STRING)!=0){
-                printf("magic string are not the same!");
+        int size_recv = recv(clntSock, client_hello, CHUNK_SIZE,0 );
+        char *pch;
+        pch  = strtok(client_hello," ");
+        while(pch != NULL){
+            if(counter == 1){
+                if(strcmp(pch, MAGIC_STRING)!=0){
+                    printf("magic string are not the same!");
+                }else{
+                    server_status = pch;
+                }
             }
-        }
-        if(counter == 2){
-            if(strcmp(pch, "HELLO") != 0){
-                printf("wrong HELLO message!");
+            if(counter == 2){
+                if(strcmp(pch, "HELLO") != 0){
+                    printf("wrong HELLO message!");
+                }
             }
-        }
-        if(counter == 3){
-            strcpy(loginid, pch);
-            printf("loginid: %s", loginid);
-        }
-        if(counter == 4){
-          int len = strlen(pch);
-          pch[len-1] =  '\0';
-          strcpy(name, pch);
-          printf("name: %s", name);
+            if(counter == 3){
+                strcpy(loginid, pch);
+            }
+            if(counter == 4){
+              int len = strlen(pch);
+              pch[len-1] =  '\0';
+              strcpy(name, pch);
+          }
+          if(counter >= 4){
+              pch = strtok(NULL," ");
+              break;
+          }
+          counter++;
+          pch = strtok(NULL, " ");
       }
-      if(counter >= 4){
-          pch = strtok(NULL," ");
-          break;
-      }
-      counter++;
-      pch = strtok(NULL, " ");
-  }
 
-  char clntName[clntlen];
-  if(inet_ntop(AF_INET, &echoClntAddr.sin_addr.s_addr, clntName,
-     clntlen) != NULL) {
-    printf("handle to %s/%d\n", clntName,echoClntAddr.sin_port);
-}
+    char clntName[clntlen];
+    if(inet_ntop(AF_INET, &echoClntAddr.sin_addr.s_addr, clntName,
+       clntlen) != NULL) {
+        printf("1ST %s HELLO %s %s %s:%d\n",MAGIC_STRING, loginid, name, clntName,echoClntAddr.sin_port);
+    }
         //generate cookie
-printf("here!");
-fflush(stdout);
 int total = 0;
 int count = 0;
 char *val = strtok(clntName, ".");
 while(val != NULL && count < 4){
-  printf("count is %d", count);
   total = total + atoi(val);
   count = count + 1;
   val = strtok(NULL, ".");
 }
-printf("here2!");
-fflush(stdout);
 cookie = (total * 13) % 1111;
+fflush(stdout);
 char cookie_str[255];
 sprintf(cookie_str, "%d", cookie);
-char * server_status = strcat(MAGIC_STRING, " STATUS ");
+fflush(stdout);
+server_status = strcat(server_status, " STATUS ");
 server_status = strcat(server_status, cookie_str);
 server_status = strcat(server_status, " ");
 server_status = strcat(server_status, inet_ntoa(echoClntAddr.sin_addr));
@@ -125,30 +124,46 @@ server_status = strcat(server_status ,":" );
 char echoServPort_str[255] ;
 sprintf(echoServPort_str, "%d",echoServPort );
 server_status = strcat(server_status,echoServPort_str );
-write(clntSock, server_status, strlen(server_status));
-	//generate client bye
-char * client_bye = malloc(255);
-int size_recv_bye = recv(clntSock, client_bye, CHUNK_SIZE,0 );
-count = 1;
-pch = strtok(client_bye," " );
-while (pch != NULL){
-    if (counter == 1 && strcmp(pch, MAGIC_STRING) != 0 ){
-      printf("Magic Strings are not the same");         
-      exit(1);         
-  }
-  if (counter == 2 &&strcmp(pch, "CLIENT_BYE") != 0 ){
-      printf(" CLIENT_BYE does not match!");
-  }    
-  if (counter == 3 && atoi(pch) !=cookie){
-      printf("Client sent back a baddddd COOOKIE");
-  }
+printf("2nd is %s\n", server_status);
+fflush(stdout);
+send(clntSock, server_status, strlen(server_status), 0);
 
-  if (counter >3){
-      pch = strtok(NULL, " ");
-      break;
-  }
-  count = count + 1;
-  pch = strtok (NULL, " ");
-}        
+//generate client bye
+char * client_bye;
+recv(clntSock, client_bye, CHUNK_SIZE,0 );
+count = 1;
+pch = strtok(client_bye," ");
+while (pch != NULL){
+    if (counter == 1 ){
+        if(strcmp(pch, MAGIC_STRING) != 0){
+            printf("Magic Strings are not the same");         
+            exit(1); 
+        }else{
+            client_bye = pch; 
+        }
+
+    }
+    if (counter == 2 &&strcmp(pch, "CLIENT_BYE") != 0 ){
+        printf(" CLIENT_BYE does not match!");
+        fflush(stdout);
+    }    
+    if (counter == 3 && atoi(pch) !=cookie){
+        printf("Client sent back a baddddd COOOKIE");
+        fflush(stdout);
+    }
+
+    if (counter >3){
+        pch = strtok(NULL, " ");
+        break;
+    }
+    count = count + 1;
+    pch = strtok (NULL, " ");
+}    
+
+
+
+
+
+
 }
 }
